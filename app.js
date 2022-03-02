f34c2b59355691a0e21325d4bd56659e1d715c5c
Shivam Thakur <80566747+shivthakur915@users.noreply.github.com>
2022-03-02 14:20:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
-import express from "express";
-import Cors from 'cors';
+const express = require('express');
 const app = express();
-const port = process.env.PORT || 8080;
-app.use(express.json());
-app.use(Cors());
 
 app.get('/', (req, res) => res.status(200).send("Hello Programmers"));
 
-app.post('/bfhl', (req, res) => {
-    const data = [...req.body.data]
+app.post('/bfhl', (req,res)=>{
 
-    let numbers = data.filter((value) => !isNaN(value))
+    const obj = {
+        is_success : true,
+        user_id: "shivam_thakur_15092000",
+        email: "shivam0240.cse19@chitkara.edu.in",
+        roll_number: 1910990240,
+        numbers: [],
+        alphabets: []
+    }
+    const {data} = req.body;
+    const responseJson = JSON.parse(data);
+    var list = (responseJson.data).length;
+    for (var i = 0; i < list; i++) 
+    {
+     var counter = responseJson.data[i];
+     if(counter >=0 && counter <=9) {
+         numbers.push(counter);
+     } else if((/[a-zA-Z]/).test(counter)) {
+         alphabets.push(counter);
+     }
+     }
+     res.json(obj);
+});
 
-    let alphabets = data.filter((value) => (/[a-zA-Z]/).test(value));
-    res.status(200).send({
-        'is_success': true,
-        'user_id': 'shivam_thakur_15092000',
-        'email': 'shivam0240.cse19@chitkara.edu.in',
-        'roll_number': '1910990240',
-        'numbers': numbers,
-        'alphabet': alphabets
-    })
-})
 
-app.listen(port, () => console.log(`listening :${port}`));
+app.listen(process.env.PORT||3000, (req,res)=>{
+    console.log("Server is running on port 3000");
+});
